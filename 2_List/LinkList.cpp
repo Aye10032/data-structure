d186beb4378d6e4978a97abdef5bc475b59de104
“Aye10032 <yeyu6_505@126.com>
2020-08-02 20:07:31 +0800

    头插法

--- 2_List/LinkList.cpp
+++ 2_List/LinkList.cpp
@@ -12,3 +12,34 @@ typedef struct LNode //结点
     struct LNode *next; //指针域
 } LNode, *LinkList;
 
+// 头插法建立单链表
+LinkList List_HeadInsert(LinkList &L)
+{
+    LNode *s;
+    int n, x;
+
+    // 创建头节点
+    L = (LinkList)malloc(sizeof(LNode));
+    L->next == NULL;
+
+    cin >> n >> x;
+    for (int i = 0; i < n; i++)
+    {
+        s = (LNode *)malloc(sizeof(LNode));
+        s->data = x;
+        s->next = L->next;
+        L->next = s;
+        cout << "new node:" << endl;
+        cin >> x;
+    }
+    return L;
+}
+
+
+int main(){
+
+    LinkList L;
+    List_HeadInsert(L);
+
+    system("pause");
+}
